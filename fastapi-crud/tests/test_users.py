888a79dc81c9973556235440f27617fad65d1e6c
redowan <redowan.nafi@gmail.com>
2022-04-16 02:31:10 +0600

    Complete rest api examples

---
+++ fastapi-crud/tests/test_users.py
@@ -0,0 +1,34 @@
+"""
+Currently these tests use the same database that the app uses.
+In a real application, you'd want to patch the 'client' to use a
+separate database.
+
+"""
+
+from http import HTTPStatus
+
+import pytest
+from httpx import AsyncClient
+
+from app.main import fast_api
+
+BASE_URL = "http://localhost:5000"
+
+
+@pytest.mark.anyio
+async def test_get_users():
+    async with AsyncClient(app=fast_api, base_url=BASE_URL) as client:
+        response = await client.get("/users")
+    assert response.status_code == HTTPStatus.OK
+
+
+# Post test doesn't work for some weird bug, most likely an edgedb client bug.
+
+# @pytest.mark.anyio
+# async def test_post_user():
+#     async with AsyncClient(app=fast_api, base_url=BASE_URL) as client:
+#         response = await client.post(
+#             "/users", json={"name": "test"}
+#         )
+#     assert response.status_code == HTTPStatus.CREATED
+#     assert response.json()["name"] == "test"
