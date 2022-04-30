1736549ab69d2bc7dd9b3a9e3292fca3dd3f5975
redowan <redowan.nafi@gmail.com>
2022-04-30 15:46:21 +0600

    Integrate edgedb

--- strawberry-gql/app/schema.py
+++ strawberry-gql/app/schema.py
@@ -1,20 +1,34 @@
 from __future__ import annotations
 
+import json
+
+import edgedb
 import strawberry
+
+client = edgedb.create_async_client()
 
 
 @strawberry.type
 class Actor:
     name: str
-    age: int
-    height: float
+    age: int | None
+    height: int | None
 
 
 @strawberry.type
 class Movie:
     name: str
-    year: int
+    year: int | None
     actors: Actor
+
+
+@strawberry.type
+class ValidationError:
+    detail: str
+
+
+ResponseActor = strawberry.union("ResponseActor", [Actor, ValidationError])
+ResponseMovie = strawberry.union("ResponseMovie", [Movie, ValidationError])
 
 
 actor = Actor(name="RDJ", age=57, height=175)  # type: ignore
@@ -28,22 +42,57 @@ movie = Movie(
 @strawberry.type
 class Query:
     @strawberry.field
-    def actors(self) -> list[Actor]:
-        return [actor]
+    def get_actors(self, filter_name: str | None) -> list[Actor]:
+        return [actor for actor in [actor] if actor.name == filter_name]
 
     @strawberry.field
-    def movies(self) -> list[Movie]:
-        return [movie]
+    def get_movies(self, filter_name: str | None) -> list[Movie]:
+        return [movie for movie in [movie] if movie.name == filter_name]
 
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def add_actor(self, name: str, age: int | None, height: float | None) -> Actor:
+    async def create_actor(
+        self, name: str, age: int | None, height: int | None
+    ) -> ResponseActor:
+        actor_json = await client.query_single_json(
+            """
+            SELECT (
+                INSERT Actor {
+                    name := <str>$name,
+                    age:=<optional int16>$age,
+                    height:=<optional int16>$height
+                }
+            ){name, age, height}
+        """,
+            name=name,
+            age=age,
+            height=height,
+        )
+
+        actor = json.loads(actor_json)
+        return Actor(actor.get("name"), actor.get("age"), actor.get("height"))
+
+    @strawberry.mutation
+    async def update_actor(
+        self, name: str, age: int | None, height: int | None
+    ) -> ResponseActor:
+        if name != "John":
+            return ValidationError("Things went wrong")
+
         return Actor(name, age, height)
 
     @strawberry.mutation
-    def add_movie(self, name: str, year: int | None, actor_names: list[str]) -> Movie:
+    async def create_movie(
+        self, name: str, year: int | None, actor_names: list[str]
+    ) -> ResponseMovie:
+        return Movie(name, year, [Actor(name) for name in actor_names])
+
+    @strawberry.mutation
+    async def update_movie(
+        self, name: str, year: int | None, actor_names: list[str]
+    ) -> ResponseMovie:
         return Movie(name, year, [Actor(name) for name in actor_names])
 
 
