03b6335895215dfb9021a63767ee546a0d62f729
redowan <redowan.nafi@gmail.com>
2022-05-01 07:46:53 +0600

    Finalized actor and movie mutations

--- strawberry-gql/app/schema.py
+++ strawberry-gql/app/schema.py
@@ -11,15 +11,15 @@ client = edgedb.create_async_client()
 @strawberry.type
 class Actor:
     name: str
-    age: int | None
-    height: int | None
+    age: int | None = None
+    height: int | None = None
 
 
 @strawberry.type
 class Movie:
     name: str
     year: int | None
-    actors: Actor
+    actors: list[Actor]
 
 
 @strawberry.type
@@ -27,27 +27,69 @@ class ValidationError:
     detail: str
 
 
-ResponseActor = strawberry.union("ResponseActor", [Actor, ValidationError])
-ResponseMovie = strawberry.union("ResponseMovie", [Movie, ValidationError])
+ResponseActor = strawberry.union("ResponseActor", (Actor, ValidationError))
+ResponseMovie = strawberry.union("ResponseMovie", (Movie, ValidationError))
 
 
-actor = Actor(name="RDJ", age=57, height=175)  # type: ignore
+actor = Actor(name="RDJ", age=57, height=175)
 movie = Movie(
     name="Avengers",
     year=2012,
     actors=[actor],
-)  # type: ignore
+)
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def get_actors(self, filter_name: str | None) -> list[Actor]:
-        return [actor for actor in [actor] if actor.name == filter_name]
+    async def get_actors(self, filter_name: str | None = None) -> list[Actor]:
+        if filter_name:
+            actors_json = await client.query_json(
+                """
+                SELECT Actor {name, age, height}
+                    FILTER .name=<str>$filter_name
+            """,
+                filter_name=filter_name,
+            )
+        else:
+            actors_json = await client.query_json(
+                """
+                SELECT Actor {name, age, height}
+            """
+            )
+        actors = json.loads(actors_json)
+
+        return [
+            Actor(name, age, height)
+            for (name, age, height) in (d.values() for d in actors)
+        ]
 
     @strawberry.field
-    def get_movies(self, filter_name: str | None) -> list[Movie]:
-        return [movie for movie in [movie] if movie.name == filter_name]
+    async def get_movies(self, filter_name: str | None) -> list[Movie]:
+        if filter_name:
+            movies_json = await client.query_json(
+                """
+                SELECT Movie {name, year, actors : {name}}
+                    FILTER .name=<str>$filter_name
+            """,
+                filter_name=filter_name,
+            )
+        else:
+            movies_json = await client.query_json(
+                """
+                SELECT Movie {name, year, actors : {name}}
+            """
+            )
+
+        # Deserialize.
+        movies = json.loads(movies_json)
+        for idx, movie in enumerate(movies):
+            actors = [
+                Actor(name) for d in movie.get("actors", []) for name in d.values()
+            ]
+            movies[idx] = Movie(movie.get("name"), movie.get("year"), actors)
+
+        return movies
 
 
 @strawberry.type
@@ -87,7 +129,31 @@ class Mutation:
     async def create_movie(
         self, name: str, year: int | None, actor_names: list[str]
     ) -> ResponseMovie:
-        return Movie(name, year, [Actor(name) for name in actor_names])
+        movie_json = await client.query_single_json(
+            """
+            WITH name:=<str>$name,
+                year:=<optional int16>$year,
+                actor_names:=<optional array<str>>$actor_names
+
+            SELECT (
+                INSERT Movie {
+                    name:=name,
+                    year:=year,
+                    actors:=(
+                        SELECT DETACHED Actor
+                        FILTER .name IN array_unpack(actor_names)
+                        )
+                }
+            ){name, year, actors : {name}}
+        """,
+            name=name,
+            year=year,
+            actor_names=actor_names,
+        )
+
+        movie = json.loads(movie_json)
+        actors = [Actor(name) for d in movie.get("actors", []) for name in d.values()]
+        return Movie(movie.get("name"), movie.get("year"), actors)
 
     @strawberry.mutation
     async def update_movie(
