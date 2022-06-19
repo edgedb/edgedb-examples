468f07de09383e7ed77b0931605a8475a0910eec
rednafi <redowan.nafi@gmail.com>
2022-06-20 01:18:33 +0600

    Fix ci

---
+++ nestjs-crud/dbschema/edgeql-js/modules/cfg.ts
@@ -0,0 +1,169 @@
+import { $ } from "edgedb";
+import * as _ from "../imports";
+import type * as _std from "./std";
+enum $AllowBareDDLλEnum {
+  AlwaysAllow = "AlwaysAllow",
+  NeverAllow = "NeverAllow",
+}
+export type $AllowBareDDL = {
+  AlwaysAllow: $.$expr_Literal<$AllowBareDDL>;
+  NeverAllow: $.$expr_Literal<$AllowBareDDL>;
+} & $.EnumType<"cfg::AllowBareDDL", `${$AllowBareDDLλEnum}`>;
+const AllowBareDDL: $AllowBareDDL = $.makeType<$AllowBareDDL>(_.spec, "98c38536-d556-11ec-95c1-8b0eb5c35864", _.syntax.literal);
+
+export type $memory = $.ScalarType<"cfg::memory", _.edgedb.ConfigMemory, true>;
+const memory: $.scalarTypeWithConstructor<$memory, never> = $.makeType<$.scalarTypeWithConstructor<$memory, never>>(_.spec, "00000000-0000-0000-0000-000000000130", _.syntax.literal);
+
+export type $ConfigObjectλShape = $.typeutil.flatten<_std.$BaseObjectλShape & {
+}>;
+type $ConfigObject = $.ObjectType<"cfg::ConfigObject", $ConfigObjectλShape, null>;
+const $ConfigObject = $.makeType<$ConfigObject>(_.spec, "9826eeca-d556-11ec-bf68-1b6406c71001", _.syntax.literal);
+
+const ConfigObject: $.$expr_PathNode<$.TypeSet<$ConfigObject, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($ConfigObject, $.Cardinality.Many), null, true);
+
+export type $AbstractConfigλShape = $.typeutil.flatten<$ConfigObjectλShape & {
+  "session_idle_timeout": $.PropertyDesc<_std.$duration, $.Cardinality.One, false, false, false, true>;
+  "session_idle_transaction_timeout": $.PropertyDesc<_std.$duration, $.Cardinality.One, false, false, false, true>;
+  "query_execution_timeout": $.PropertyDesc<_std.$duration, $.Cardinality.One, false, false, false, false>;
+  "listen_port": $.PropertyDesc<_std.$int16, $.Cardinality.One, false, false, false, true>;
+  "listen_addresses": $.PropertyDesc<_std.$str, $.Cardinality.Many, false, false, false, false>;
+  "auth": $.LinkDesc<$Auth, $.Cardinality.Many, {}, false, false,  false, false>;
+  "allow_dml_in_functions": $.PropertyDesc<_std.$bool, $.Cardinality.AtMostOne, false, false, false, true>;
+  "allow_bare_ddl": $.PropertyDesc<$AllowBareDDL, $.Cardinality.AtMostOne, false, false, false, true>;
+  "shared_buffers": $.PropertyDesc<$memory, $.Cardinality.AtMostOne, false, false, false, false>;
+  "query_work_mem": $.PropertyDesc<$memory, $.Cardinality.AtMostOne, false, false, false, false>;
+  "effective_cache_size": $.PropertyDesc<$memory, $.Cardinality.AtMostOne, false, false, false, false>;
+  "effective_io_concurrency": $.PropertyDesc<_std.$int64, $.Cardinality.AtMostOne, false, false, false, false>;
+  "default_statistics_target": $.PropertyDesc<_std.$int64, $.Cardinality.AtMostOne, false, false, false, false>;
+}>;
+type $AbstractConfig = $.ObjectType<"cfg::AbstractConfig", $AbstractConfigλShape, null>;
+const $AbstractConfig = $.makeType<$AbstractConfig>(_.spec, "98e63c22-d556-11ec-8990-b54fa361c828", _.syntax.literal);
+
+const AbstractConfig: $.$expr_PathNode<$.TypeSet<$AbstractConfig, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($AbstractConfig, $.Cardinality.Many), null, true);
+
+export type $AuthλShape = $.typeutil.flatten<$ConfigObjectλShape & {
+  "priority": $.PropertyDesc<_std.$int64, $.Cardinality.One, true, false, true, false>;
+  "user": $.PropertyDesc<_std.$str, $.Cardinality.Many, false, false, true, true>;
+  "method": $.LinkDesc<$AuthMethod, $.Cardinality.AtMostOne, {}, true, false,  false, false>;
+  "comment": $.PropertyDesc<_std.$str, $.Cardinality.AtMostOne, false, false, true, false>;
+  "<auth[is cfg::AbstractConfig]": $.LinkDesc<$AbstractConfig, $.Cardinality.Many, {}, false, false,  false, false>;
+  "<auth[is cfg::Config]": $.LinkDesc<$Config, $.Cardinality.Many, {}, false, false,  false, false>;
+  "<auth[is cfg::InstanceConfig]": $.LinkDesc<$InstanceConfig, $.Cardinality.Many, {}, false, false,  false, false>;
+  "<auth[is cfg::DatabaseConfig]": $.LinkDesc<$DatabaseConfig, $.Cardinality.Many, {}, false, false,  false, false>;
+  "<auth": $.LinkDesc<$.ObjectType, $.Cardinality.Many, {}, false, false,  false, false>;
+}>;
+type $Auth = $.ObjectType<"cfg::Auth", $AuthλShape, null>;
+const $Auth = $.makeType<$Auth>(_.spec, "98c50350-d556-11ec-b11b-3bae56ff69f7", _.syntax.literal);
+
+const Auth: $.$expr_PathNode<$.TypeSet<$Auth, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($Auth, $.Cardinality.Many), null, true);
+
+export type $AuthMethodλShape = $.typeutil.flatten<$ConfigObjectλShape & {
+  "<method[is cfg::Auth]": $.LinkDesc<$Auth, $.Cardinality.AtMostOne, {}, true, false,  false, false>;
+  "<method": $.LinkDesc<$.ObjectType, $.Cardinality.Many, {}, false, false,  false, false>;
+}>;
+type $AuthMethod = $.ObjectType<"cfg::AuthMethod", $AuthMethodλShape, null>;
+const $AuthMethod = $.makeType<$AuthMethod>(_.spec, "983604e7-d556-11ec-b351-2fd1cd651746", _.syntax.literal);
+
+const AuthMethod: $.$expr_PathNode<$.TypeSet<$AuthMethod, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($AuthMethod, $.Cardinality.Many), null, true);
+
+export type $ConfigλShape = $.typeutil.flatten<$AbstractConfigλShape & {
+}>;
+type $Config = $.ObjectType<"cfg::Config", $ConfigλShape, null>;
+const $Config = $.makeType<$Config>(_.spec, "991b44be-d556-11ec-9ecb-cdb05513c16c", _.syntax.literal);
+
+const Config: $.$expr_PathNode<$.TypeSet<$Config, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($Config, $.Cardinality.Many), null, true);
+
+export type $DatabaseConfigλShape = $.typeutil.flatten<$AbstractConfigλShape & {
+}>;
+type $DatabaseConfig = $.ObjectType<"cfg::DatabaseConfig", $DatabaseConfigλShape, null>;
+const $DatabaseConfig = $.makeType<$DatabaseConfig>(_.spec, "998a1c9e-d556-11ec-be34-433c8c4a659d", _.syntax.literal);
+
+const DatabaseConfig: $.$expr_PathNode<$.TypeSet<$DatabaseConfig, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($DatabaseConfig, $.Cardinality.Many), null, true);
+
+export type $InstanceConfigλShape = $.typeutil.flatten<$AbstractConfigλShape & {
+}>;
+type $InstanceConfig = $.ObjectType<"cfg::InstanceConfig", $InstanceConfigλShape, null>;
+const $InstanceConfig = $.makeType<$InstanceConfig>(_.spec, "9952bb96-d556-11ec-8f8e-9524e224c00b", _.syntax.literal);
+
+const InstanceConfig: $.$expr_PathNode<$.TypeSet<$InstanceConfig, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($InstanceConfig, $.Cardinality.Many), null, true);
+
+export type $SCRAMλShape = $.typeutil.flatten<$AuthMethodλShape & {
+}>;
+type $SCRAM = $.ObjectType<"cfg::SCRAM", $SCRAMλShape, null>;
+const $SCRAM = $.makeType<$SCRAM>(_.spec, "98540bad-d556-11ec-9c74-5b8fa83d5afa", _.syntax.literal);
+
+const SCRAM: $.$expr_PathNode<$.TypeSet<$SCRAM, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($SCRAM, $.Cardinality.Many), null, true);
+
+export type $TrustλShape = $.typeutil.flatten<$AuthMethodλShape & {
+}>;
+type $Trust = $.ObjectType<"cfg::Trust", $TrustλShape, null>;
+const $Trust = $.makeType<$Trust>(_.spec, "9844f07d-d556-11ec-9cbb-636f3da4842a", _.syntax.literal);
+
+const Trust: $.$expr_PathNode<$.TypeSet<$Trust, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($Trust, $.Cardinality.Many), null, true);
+
+type get_config_jsonλFuncExpr<
+  NamedArgs extends {
+    "sources"?: $.TypeSet<$.ArrayType<_std.$str>>,
+    "max_source"?: _.castMaps.orScalarLiteral<$.TypeSet<_std.$str>>,
+  },
+> = $.$expr_Function<
+  "cfg::get_config_json",
+  [],
+  _.castMaps.mapLiteralToTypeSet<NamedArgs>,
+  $.TypeSet<_std.$json, $.cardinalityUtil.multiplyCardinalities<$.cardinalityUtil.optionalParamCardinality<NamedArgs["sources"]>, $.cardinalityUtil.optionalParamCardinality<_.castMaps.literalToTypeSet<NamedArgs["max_source"]>>>>
+>;
+function get_config_json<
+  NamedArgs extends {
+    "sources"?: $.TypeSet<$.ArrayType<_std.$str>>,
+    "max_source"?: _.castMaps.orScalarLiteral<$.TypeSet<_std.$str>>,
+  },
+>(
+  namedArgs: NamedArgs,
+): get_config_jsonλFuncExpr<NamedArgs>;
+function get_config_json(...args: any[]) {
+  const {returnType, cardinality, args: positionalArgs, namedArgs} = _.syntax.$resolveOverload('cfg::get_config_json', args, _.spec, [
+    {args: [], namedArgs: {"sources": {typeId: "05f91774-15ea-9001-038e-092c1cad80af", optional: true, setoftype: false, variadic: false}, "max_source": {typeId: "00000000-0000-0000-0000-000000000101", optional: true, setoftype: false, variadic: false}}, returnTypeId: "00000000-0000-0000-0000-00000000010f"},
+  ]);
+  return _.syntax.$expressionify({
+    __kind__: $.ExpressionKind.Function,
+    __element__: returnType,
+    __cardinality__: cardinality,
+    __name__: "cfg::get_config_json",
+    __args__: positionalArgs,
+    __namedargs__: namedArgs,
+  }) as any;
+};
+
+
+
+export { $AllowBareDDLλEnum, AllowBareDDL, memory, $ConfigObject, ConfigObject, $AbstractConfig, AbstractConfig, $Auth, Auth, $AuthMethod, AuthMethod, $Config, Config, $DatabaseConfig, DatabaseConfig, $InstanceConfig, InstanceConfig, $SCRAM, SCRAM, $Trust, Trust };
+
+type __defaultExports = {
+  "AllowBareDDL": typeof AllowBareDDL;
+  "memory": typeof memory;
+  "ConfigObject": typeof ConfigObject;
+  "AbstractConfig": typeof AbstractConfig;
+  "Auth": typeof Auth;
+  "AuthMethod": typeof AuthMethod;
+  "Config": typeof Config;
+  "DatabaseConfig": typeof DatabaseConfig;
+  "InstanceConfig": typeof InstanceConfig;
+  "SCRAM": typeof SCRAM;
+  "Trust": typeof Trust;
+  "get_config_json": typeof get_config_json
+};
+const __defaultExports: __defaultExports = {
+  "AllowBareDDL": AllowBareDDL,
+  "memory": memory,
+  "ConfigObject": ConfigObject,
+  "AbstractConfig": AbstractConfig,
+  "Auth": Auth,
+  "AuthMethod": AuthMethod,
+  "Config": Config,
+  "DatabaseConfig": DatabaseConfig,
+  "InstanceConfig": InstanceConfig,
+  "SCRAM": SCRAM,
+  "Trust": Trust,
+  "get_config_json": get_config_json
+};
+export default __defaultExports;
---
+++ nestjs-crud/dbschema/edgeql-js/modules/sys.ts
@@ -0,0 +1,195 @@
+import { $ } from "edgedb";
+import * as _ from "../imports";
+import type * as _schema from "./schema";
+import type * as _std from "./std";
+enum $TransactionIsolationλEnum {
+  RepeatableRead = "RepeatableRead",
+  Serializable = "Serializable",
+}
+export type $TransactionIsolation = {
+  RepeatableRead: $.$expr_Literal<$TransactionIsolation>;
+  Serializable: $.$expr_Literal<$TransactionIsolation>;
+} & $.EnumType<"sys::TransactionIsolation", `${$TransactionIsolationλEnum}`>;
+const TransactionIsolation: $TransactionIsolation = $.makeType<$TransactionIsolation>(_.spec, "9734f19b-d556-11ec-af14-a5b642d3b3da", _.syntax.literal);
+
+enum $VersionStageλEnum {
+  dev = "dev",
+  alpha = "alpha",
+  beta = "beta",
+  rc = "rc",
+  final = "final",
+}
+export type $VersionStage = {
+  dev: $.$expr_Literal<$VersionStage>;
+  alpha: $.$expr_Literal<$VersionStage>;
+  beta: $.$expr_Literal<$VersionStage>;
+  rc: $.$expr_Literal<$VersionStage>;
+  final: $.$expr_Literal<$VersionStage>;
+} & $.EnumType<"sys::VersionStage", `${$VersionStageλEnum}`>;
+const VersionStage: $VersionStage = $.makeType<$VersionStage>(_.spec, "9735bd5e-d556-11ec-8845-29687743757d", _.syntax.literal);
+
+export type $SystemObjectλShape = $.typeutil.flatten<_schema.$AnnotationSubjectλShape & {
+}>;
+type $SystemObject = $.ObjectType<"sys::SystemObject", $SystemObjectλShape, null>;
+const $SystemObject = $.makeType<$SystemObject>(_.spec, "9736778d-d556-11ec-a9a2-d7dbf7e925dc", _.syntax.literal);
+
+const SystemObject: $.$expr_PathNode<$.TypeSet<$SystemObject, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($SystemObject, $.Cardinality.Many), null, true);
+
+export type $DatabaseλShape = $.typeutil.flatten<$SystemObjectλShape & _schema.$AnnotationSubjectλShape & {
+  "name": $.PropertyDesc<_std.$str, $.Cardinality.One, true, false, false, false>;
+}>;
+type $Database = $.ObjectType<"sys::Database", $DatabaseλShape, null>;
+const $Database = $.makeType<$Database>(_.spec, "975de3a6-d556-11ec-8e4d-c3513e265aec", _.syntax.literal);
+
+const Database: $.$expr_PathNode<$.TypeSet<$Database, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($Database, $.Cardinality.Many), null, true);
+
+export type $ExtensionPackageλShape = $.typeutil.flatten<$SystemObjectλShape & _schema.$AnnotationSubjectλShape & {
+  "script": $.PropertyDesc<_std.$str, $.Cardinality.One, false, false, false, false>;
+  "version": $.PropertyDesc<$.NamedTupleType<{major: _std.$int64, minor: _std.$int64, stage: $VersionStage, stage_no: _std.$int64, local: $.ArrayType<_std.$str>}>, $.Cardinality.One, false, false, false, false>;
+  "<package[is schema::Extension]": $.LinkDesc<_schema.$Extension, $.Cardinality.AtMostOne, {}, true, false,  false, false>;
+  "<package": $.LinkDesc<$.ObjectType, $.Cardinality.Many, {}, false, false,  false, false>;
+}>;
+type $ExtensionPackage = $.ObjectType<"sys::ExtensionPackage", $ExtensionPackageλShape, null>;
+const $ExtensionPackage = $.makeType<$ExtensionPackage>(_.spec, "978a2713-d556-11ec-93db-b9c3935d1483", _.syntax.literal);
+
+const ExtensionPackage: $.$expr_PathNode<$.TypeSet<$ExtensionPackage, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($ExtensionPackage, $.Cardinality.Many), null, true);
+
+export type $RoleλShape = $.typeutil.flatten<$SystemObjectλShape & _schema.$InheritingObjectλShape & _schema.$AnnotationSubjectλShape & {
+  "name": $.PropertyDesc<_std.$str, $.Cardinality.One, true, false, false, false>;
+  "superuser": $.PropertyDesc<_std.$bool, $.Cardinality.One, false, false, false, false>;
+  "is_superuser": $.PropertyDesc<_std.$bool, $.Cardinality.One, false, true, false, false>;
+  "password": $.PropertyDesc<_std.$str, $.Cardinality.AtMostOne, false, false, false, false>;
+  "member_of": $.LinkDesc<$Role, $.Cardinality.Many, {}, false, false,  false, false>;
+  "<member_of[is sys::Role]": $.LinkDesc<$Role, $.Cardinality.Many, {}, false, false,  false, false>;
+  "<member_of": $.LinkDesc<$.ObjectType, $.Cardinality.Many, {}, false, false,  false, false>;
+}>;
+type $Role = $.ObjectType<"sys::Role", $RoleλShape, null>;
+const $Role = $.makeType<$Role>(_.spec, "97bc2eea-d556-11ec-88ee-d392a83cc059", _.syntax.literal);
+
+const Role: $.$expr_PathNode<$.TypeSet<$Role, $.Cardinality.Many>, null, true> = _.syntax.$PathNode($.$toSet($Role, $.Cardinality.Many), null, true);
+
+type get_versionλFuncExpr = $.$expr_Function<
+  "sys::get_version",
+  [],
+  {},
+  $.TypeSet<$.NamedTupleType<{major: _std.$int64, minor: _std.$int64, stage: $VersionStage, stage_no: _std.$int64, local: $.ArrayType<_std.$str>}>, $.Cardinality.One>
+>;
+/**
+ * Return the server version as a tuple.
+ */
+function get_version(): get_versionλFuncExpr;
+function get_version(...args: any[]) {
+  const {returnType, cardinality, args: positionalArgs, namedArgs} = _.syntax.$resolveOverload('sys::get_version', args, _.spec, [
+    {args: [], returnTypeId: "4f04e529-c585-fdcd-d4ee-ec50d5695ef6"},
+  ]);
+  return _.syntax.$expressionify({
+    __kind__: $.ExpressionKind.Function,
+    __element__: returnType,
+    __cardinality__: cardinality,
+    __name__: "sys::get_version",
+    __args__: positionalArgs,
+    __namedargs__: namedArgs,
+  }) as any;
+};
+
+type get_version_as_strλFuncExpr = $.$expr_Function<
+  "sys::get_version_as_str",
+  [],
+  {},
+  $.TypeSet<_std.$str, $.Cardinality.One>
+>;
+/**
+ * Return the server version as a string.
+ */
+function get_version_as_str(): get_version_as_strλFuncExpr;
+function get_version_as_str(...args: any[]) {
+  const {returnType, cardinality, args: positionalArgs, namedArgs} = _.syntax.$resolveOverload('sys::get_version_as_str', args, _.spec, [
+    {args: [], returnTypeId: "00000000-0000-0000-0000-000000000101"},
+  ]);
+  return _.syntax.$expressionify({
+    __kind__: $.ExpressionKind.Function,
+    __element__: returnType,
+    __cardinality__: cardinality,
+    __name__: "sys::get_version_as_str",
+    __args__: positionalArgs,
+    __namedargs__: namedArgs,
+  }) as any;
+};
+
+type get_transaction_isolationλFuncExpr = $.$expr_Function<
+  "sys::get_transaction_isolation",
+  [],
+  {},
+  $.TypeSet<$TransactionIsolation, $.Cardinality.One>
+>;
+/**
+ * Return the isolation level of the current transaction.
+ */
+function get_transaction_isolation(): get_transaction_isolationλFuncExpr;
+function get_transaction_isolation(...args: any[]) {
+  const {returnType, cardinality, args: positionalArgs, namedArgs} = _.syntax.$resolveOverload('sys::get_transaction_isolation', args, _.spec, [
+    {args: [], returnTypeId: "9734f19b-d556-11ec-af14-a5b642d3b3da"},
+  ]);
+  return _.syntax.$expressionify({
+    __kind__: $.ExpressionKind.Function,
+    __element__: returnType,
+    __cardinality__: cardinality,
+    __name__: "sys::get_transaction_isolation",
+    __args__: positionalArgs,
+    __namedargs__: namedArgs,
+  }) as any;
+};
+
+type get_current_databaseλFuncExpr = $.$expr_Function<
+  "sys::get_current_database",
+  [],
+  {},
+  $.TypeSet<_std.$str, $.Cardinality.One>
+>;
+/**
+ * Return the name of the current database as a string.
+ */
+function get_current_database(): get_current_databaseλFuncExpr;
+function get_current_database(...args: any[]) {
+  const {returnType, cardinality, args: positionalArgs, namedArgs} = _.syntax.$resolveOverload('sys::get_current_database', args, _.spec, [
+    {args: [], returnTypeId: "00000000-0000-0000-0000-000000000101"},
+  ]);
+  return _.syntax.$expressionify({
+    __kind__: $.ExpressionKind.Function,
+    __element__: returnType,
+    __cardinality__: cardinality,
+    __name__: "sys::get_current_database",
+    __args__: positionalArgs,
+    __namedargs__: namedArgs,
+  }) as any;
+};
+
+
+
+export { $TransactionIsolationλEnum, TransactionIsolation, $VersionStageλEnum, VersionStage, $SystemObject, SystemObject, $Database, Database, $ExtensionPackage, ExtensionPackage, $Role, Role };
+
+type __defaultExports = {
+  "TransactionIsolation": typeof TransactionIsolation;
+  "VersionStage": typeof VersionStage;
+  "SystemObject": typeof SystemObject;
+  "Database": typeof Database;
+  "ExtensionPackage": typeof ExtensionPackage;
+  "Role": typeof Role;
+  "get_version": typeof get_version;
+  "get_version_as_str": typeof get_version_as_str;
+  "get_transaction_isolation": typeof get_transaction_isolation;
+  "get_current_database": typeof get_current_database
+};
+const __defaultExports: __defaultExports = {
+  "TransactionIsolation": TransactionIsolation,
+  "VersionStage": VersionStage,
+  "SystemObject": SystemObject,
+  "Database": Database,
+  "ExtensionPackage": ExtensionPackage,
+  "Role": Role,
+  "get_version": get_version,
+  "get_version_as_str": get_version_as_str,
+  "get_transaction_isolation": get_transaction_isolation,
+  "get_current_database": get_current_database
+};
+export default __defaultExports;
