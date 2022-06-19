44b8b81518d1f4e6e95ba8538b3a57d83265f5f0
rednafi <redowan.nafi@gmail.com>
2022-06-19 23:58:33 +0600

    Final

--- nestjs-crud/src/main.ts
+++ nestjs-crud/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle("Actors example")
+    .setDescription("The actors API description")
+    .setVersion("1.0")
+    .addTag("actors")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
