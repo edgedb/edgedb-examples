20216819b86b8c4d96d2e276e8f914833dcfec66
Colin McDonnell <colin@edgedb.com>
2022-07-19 20:09:49 -0700

    Update deps

--- nextjs-todo/pages/_app.tsx
+++ nextjs-todo/pages/_app.tsx
@@ -1,8 +1,8 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import type {AppProps /*, AppContext */} from 'next/app';
+import {useState} from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
