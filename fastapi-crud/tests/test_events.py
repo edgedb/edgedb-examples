2cf1e862946851c6807caa5e1cd326ce19af5770
Devon Campbell <devon@edgedb.com>
2023-02-10 14:34:36 -0800

    Add more test cases

--- fastapi-crud/tests/test_events.py
+++ fastapi-crud/tests/test_events.py
@@ -6,13 +6,83 @@ separate database.
 """
 
 from http import HTTPStatus
+import uuid
+import datetime
 
 from fastapi.testclient import TestClient
 
 from app.main import fast_api
+from app.queries import get_events_async_edgeql as get_events_qry
 
 
 def test_get_events():
     with TestClient(fast_api) as client:
         response = client.get("/events")
     assert response.status_code == HTTPStatus.OK
+
+def test_get_events_with_single_event(mocker):
+    event_name = "Test"
+    event_address = "Address"
+    event_host = "Test Host"
+    mocker.patch(
+        "app.events.get_events_qry.get_events",
+        return_value=[
+            get_events_qry.GetEventsResult(
+                id=uuid.uuid4(),
+                name=event_name,
+                address=event_address,
+                host=get_events_qry.GetEventsResultHost(id=uuid.uuid4(), name=event_host),
+                schedule=datetime.datetime.now()
+            )
+        ]
+    )
+    with TestClient(fast_api) as client:
+        response = client.get("/events")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()[0]["name"] == event_name
+
+def test_get_events_with_multiple_events(mocker):
+    event_1_name = "Test 1"
+    event_2_name = "Test 2"
+    event_1_address = "Address"
+    event_2_address = "Address"
+    event_1_host = "Test Host"
+    event_2_host = "Test Host"
+    mocker.patch(
+        "app.events.get_events_qry.get_events",
+        return_value=[
+            get_events_qry.GetEventsResult(
+                id=uuid.uuid4(),
+                name=event_1_name,
+                address=event_1_address,
+                host=get_events_qry.GetEventsResultHost(id=uuid.uuid4(), name=event_1_host),
+                schedule=datetime.datetime.now()
+            ),
+            get_events_qry.GetEventsResult(
+                id=uuid.uuid4(),
+                name=event_2_name,
+                address=event_2_address,
+                host=get_events_qry.GetEventsResultHost(id=uuid.uuid4(), name=event_2_host),
+                schedule=datetime.datetime.now()
+            )
+        ]
+    )
+    with TestClient(fast_api) as client:
+        response = client.get("/events")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()[0]["name"] == event_1_name
+    assert response.json()[1]["name"] == event_2_name
+
+def test_post_event():
+    with TestClient(fast_api) as client:
+        response = client.post(
+            "/events",
+            json={
+                "name": "test",
+                "address": "test address",
+                "host_name": "Test",
+                "schedule": "2010-12-27T23:59:59-07:00"
+            }
+        )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json()["name"] == "test"
--- fastapi-crud/tests/test_users.py
+++ fastapi-crud/tests/test_users.py
@@ -6,10 +6,13 @@ separate database.
 """
 
 from http import HTTPStatus
+import uuid
+import datetime
 
 from fastapi.testclient import TestClient
 
 from app.main import fast_api
+from app.queries import get_users_async_edgeql as get_users_qry
 
 
 def test_get_users():
@@ -17,6 +20,46 @@ def test_get_users():
         response = client.get("/users")
     assert response.status_code == HTTPStatus.OK
 
+def test_get_users_with_single_user(mocker):
+    user_name = "Test"
+    mocker.patch(
+        "app.users.get_users_qry.get_users",
+        return_value=[
+            get_users_qry.GetUsersResult(
+                id=uuid.uuid4(),
+                name=user_name,
+                created_at=datetime.datetime.now()
+            )
+        ]
+    )
+    with TestClient(fast_api) as client:
+        response = client.get("/users")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()[0]["name"] == user_name
+
+def test_get_users_with_multiple_users(mocker):
+    user_1_name = "Test 1"
+    user_2_name = "Test 2"
+    mocker.patch(
+        "app.users.get_users_qry.get_users",
+        return_value=[
+            get_users_qry.GetUsersResult(
+                id=uuid.uuid4(),
+                name=user_1_name,
+                created_at=datetime.datetime.now()
+            ),
+            get_users_qry.GetUsersResult(
+                id=uuid.uuid4(),
+                name=user_2_name,
+                created_at=datetime.datetime.now()
+            )
+        ]
+    )
+    with TestClient(fast_api) as client:
+        response = client.get("/users")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()[0]["name"] == user_1_name
+    assert response.json()[1]["name"] == user_2_name
 
 def test_post_user():
     with TestClient(fast_api) as client:
