438f5a2101aa7ce7775ca482f0db8b3d0f41acd2
Dijana Pavlovic <dijana@edgedb.com>
2023-12-14 14:02:22 -0600

    Reset add todo form state after submitting

--- remix-auth/app/components/todos/TodoList.tsx
+++ remix-auth/app/components/todos/TodoList.tsx
@@ -1,15 +1,31 @@
 import { SendIcon } from "~/icons";
-import { Form } from "@remix-run/react";
+import { Form, useNavigation } from "@remix-run/react";
 import { type Todo, TodoCard } from "./TodoCard";
+import { useEffect, useRef } from "react";
 
 export function TodoList({ todos }: { todos: Todo[] }) {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isSubmitting) return;
+    formRef.current?.reset();
+    inputRef.current?.focus();
+  }, [isSubmitting]);
+
   return (
     <div className="flex flex-col gap-3 mt-4">
       <Form
+        ref={formRef}
         method="post"
         className="flex bg-slate-50 border border-slate-200 rounded-xl mb-4 overflow-hidden outline-teal-600 outline-[3px] focus-within:outline focus-within:bg-white"
       >
         <input
+          ref={inputRef}
           type="text"
           name="newTodo"
           placeholder="Add new todo..."
